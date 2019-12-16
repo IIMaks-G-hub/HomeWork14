fe533f9bcf13cc3a47e11d37083d1f0fb4b74d5b
Irina <57173844+IIMaks-G-hub@users.noreply.github.com>
2019-12-16 14:08:23 +0300

    first commit

--- code.js
+++ code.js
@@ -1,10 +1,32 @@
-
+let main_picture = 0;
 $('.main_picture').slick({
     slidesToScroll: 1,
     autoplaySpeed: 1000,
     slidesToShow: 3,
-    autoplay: true,
+    arrows: true,
 }
-    
-);
 
+);
+$(function() {
+    function log( text ) {
+    $('#log').text( $('#log').text() + "\n" + text )  
+  }
+  function evLeft() {
+    console.log('event left')
+  }
+  function evRight() {
+    console.log('event right')  
+  }
+    $(window).on('keydown' , function( e ) {
+    switch( e.keyCode ) {
+        case 37:
+        evLeft();
+        break;
+      case 39:
+        evRight();
+        break;
+    }
+  })
+  $('.left').on('click' , evLeft )
+  $('.right').on('click' , evRight )
+})
